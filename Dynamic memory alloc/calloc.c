7abfe27632fb857c4f05d21bd27a90837ec9f107
durvesiSrinivas <100716243+durvesiSrinivas@users.noreply.github.com>
2023-05-31 20:11:49 +0530

    Add files via upload

---
+++ Dynamic memory alloc/calloc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int  main(){
+int *ptr, i, n=5;
+
+ptr= (int *) calloc (n, sizeof(int));
+
+printf("enter elements of dynamic memory:\n");
+for(i=0; i<n; i++){
+    scanf("%d", (ptr+i));
+    }    
+
+printf("elements of dynamic memory are:\n");
+    for(i=0; i<n; i++){
+    printf("%d ", *(ptr+i));
+    }    
+            
+    return 0;
+    }
---
+++ Dynamic memory alloc/malloc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+# include<stdlib.h>
+
+int main(){
+
+int *ptr, i, n=5;
+
+    ptr= (int *) malloc(sizeof( int ) * n);    
+   printf("enter elements of array:\n");
+       for(i=0; i<n; i++){
+            scanf("%d", (ptr+i));
+            }
+   printf("elements of dynamic malloc :\n");
+             for(i=0; i<n; i++){
+                  printf("%d ", *(ptr+i));
+              }         
+                    
+    
+    return 0;
+    }
